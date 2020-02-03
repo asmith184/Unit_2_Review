10b1d5f0757ba00a97927131fd6e35dbb0efbb26
Aspen <asmith184@west-mec.org>
2020-02-03 14:21:21 -0700

    finished review

--- lht_table.js
+++ lht_table.js
@@ -11,3 +11,27 @@
 	
 */
 
+//var created to test script
+var thisDay = new Date("August 30, 2018");
+
+// ------
+var tableHTML = "<table id='eventTable'> <caption>Upcoming Events</caption> <tr><th>Date</th><th>Event</th><th>Price</th></tr>";
+
+// ------
+var endDate = new Date(thisDay.getTime() + 14 * 24 * 60 * 60 * 1000)
+
+//for loop that loops through the length of eventDates array
+for (var i = 0; i < eventDates.length; i++) {
+   var eventDate = new Date(eventDates[i]);
+   var eventDay = eventDate.toDateString();
+   var eventTime = eventDate.toLocaleTimeString();
+   //stating that if thisDay <= eventDate && eventDate <= endDate, code in if statement will run
+   if (thisDay <= eventDate && eventDate <= endDate){
+       tableHTML += "<tr> <td>" + eventDay + " @ " + eventTime + "</td> <td>" + eventDescriptions[i] + "</td> <td>" + eventPrices[i] + "</td> </tr>";
+   }
+}
+
+//adding code to the tablehtml variable
+tableHTML += "</table>";
+
+document.getElementById("eventList").innerHTML = tableHTML;
